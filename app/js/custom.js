94ce635a5a6dd824cc224a9b10e8d2700ddc3e81
Tej <tejpartapsingh91@gmail.com>
2016-10-22 11:10:48 -0400

    Simplified Counter function on multiple Sliders.

--- app/js/custom.js
+++ app/js/custom.js
@@ -13,12 +13,9 @@ $(document).ready(function() {
 	$(".partition").each(function() {
 		var temp = $(this).val();
 		$(this).siblings(".slider").find(".counter").append(temp);
+		$(this).on("change slide", function (e) {
+	    $(this).siblings(".slider").find(".counter").text(e.value.newValue);
+		}); // Source: https://github.com/seiyria/bootstrap-slider/issues/336
 	});
-	$(".partition").on("slide", function(slideEvt) {
-		$(this).siblings(".slider").find(".counter").text(slideEvt.value);
-	});
-	$("#parent-partition, #gpu-partition, #gpu2-partition").on("change", function (e) {
-    $(this).siblings(".slider").find(".counter").text(e.value.newValue);
-	}); // Source: https://github.com/seiyria/bootstrap-slider/issues/336
 
 });
