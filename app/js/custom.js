f7ffa43cbcaeb7ebe97492e2b3dc12190088622f
Tej <tejpartapsingh91@gmail.com>
2016-10-22 10:25:45 -0400

    Added documentation, semantic structure.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+
 	$(".partition").slider();
+	// Declare Progress Slider for .partition Inputs.
+
 	$(".slider-handle").append("<div class='counter'></div>");
-	$(".button-group .slider-disabled").append("<span class='off'>GPU Partitions have been individually customized.</span>")
+	// Declare a placeholder to show Indicator.
+
+	$("#primary-slider .slider-disabled").append("<span class='off'>GPU Partitions have been individually customized.</span>");
+	// Add a placeholder text below Primary Slider.
+
+	// Update Counter value.
 	$(".partition").each(function() {
 		var temp = $(this).val();
 		$(this).siblings(".slider").find(".counter").append(temp);
@@ -12,4 +20,5 @@ $(document).ready(function() {
 	$("#parent-partition, #gpu-partition, #gpu2-partition").on("change", function (e) {
     $(this).siblings(".slider").find(".counter").text(e.value.newValue);
 	}); // Source: https://github.com/seiyria/bootstrap-slider/issues/336
+
 });
